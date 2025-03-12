36aafcc024475d7e7d77b64672bf5fba4492f6e0
devamole <devangelmotta@gmail.com>
2025-03-12 02:34:28 -0500

    +++

--- src/test/java/com/credibanco/transactions/infrastructure/repository/TransactionJpaRepositoryTest.java
+++ src/test/java/com/credibanco/transactions/infrastructure/repository/TransactionJpaRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class TransactionJpaRepositoryTest {
 
     @Autowired
